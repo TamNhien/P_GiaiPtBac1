31445feb390845fb7b4187e4562162997a4773bb
TamNhien <canhthien91@gmail.com>
2024-03-21 20:42:02 +0700

    Add project files.

---
+++ P_GiaiPtBac1.py
@@ -0,0 +1,13 @@
+﻿a = float(input("Nhập giá trị của a : "))
+b = float(input("Nhập giá trị của b : "))
+
+# Kiểm tra a có bằng 0 không để tránh chia cho 0
+if a == 0:
+    if b == 0:
+        print("Phương trình có vô số nghiệm")
+    else:
+        print("Phương trình vô nghiệm")
+else:
+    x = -b / a
+    print("Nghiệm của phương trình là : ", x)
+
